929e2597733f6c5663f4ea3e2f3ebb03f0764796
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-05-06 01:11:55 +0300

    extracted cli app login to separate class

--- server/CLIApp.py
+++ server/CLIApp.py
@@ -1,3 +1,71 @@
+import utils
+import openai
+import numpy as np
+import pandas as pd
+from openai.embeddings_utils import cosine_similarity, get_embedding
+
+
 class CLIApp:
-    def __init__(self):
-        pass
+    def __init__(self, output: str, filePath: str):
+        self.__output = output
+        self.__filePath = filePath
+
+    def get_embedding(self, text, model="text-embedding-ada-002"):
+        return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
+
+    def search(self, df, searchTerm, n=3, pprint=True):
+        queryEmbedding = self.get_embedding(
+            searchTerm, model='text-embedding-ada-002')
+        df['similarities'] = df.embedding.apply(
+            lambda x: cosine_similarity(x, queryEmbedding))
+        res = df.sort_values('similarities', ascending=False).head(n)
+        return res.iloc[0]['sentence']
+
+    # This processes the document text and for each paragraph, it makes a call to the OpenAI embeddings API
+    # to get the embeddings for each paragraph. We put it in a pandas dataframe in the embedding column.
+    # Then we are ready to save it to a CSV file.
+    def process_sentences(self):
+        """ First ypu want to get the sentences from the file with the contents """
+        sentences = utils.get_sentences(self.__filePath)
+        """ Then you want to get the embeddings for each sentence and place it in a pandas dataframe """
+
+        columns = ["sentence", "embedding"]
+        dataframe = pd.DataFrame(columns=columns)
+
+        dataframe["sentence"] = sentences
+        dataframe["embedding"] = dataframe["sentence"].apply(
+            self.get_embedding)
+
+        dataframe.to_csv(self.__output, index=False)
+
+        print("Done! Check the output file: ", self.__output)
+
+    def process_search(self):
+        """ Read the output file and get the embeddings """
+        dataframe = pd.read_csv(self.__output)
+
+        """ Get the search term """
+        searchTerm = input("Enter a search term: ")
+        dataframe['embedding'] = dataframe.embedding.apply(
+            eval).apply(np.array)
+        res = self.search(dataframe, searchTerm)
+
+        print(res)
+
+    def run(self):
+        while True:
+            command = input("Enter a command: ")
+            if int(command) == 1:
+                self.process_sentences()
+            elif int(command) == 2:
+                self.process_search()
+            elif int(command) == 0:
+                print("\n\nBye!\n\n")
+                break
+
+
+if __name__ == "__main__":
+    filePath = "./speech-transcript.pdf"
+    outputName = "output.csv"
+    cliApp = CLIApp(outputName, filePath)
+    cliApp.run()
--- server/main.py
+++ server/main.py
@@ -1,5 +1,3 @@
-import csv
-import json
 import utils
 import openai
 import numpy as np
@@ -7,54 +5,8 @@ import pandas as pd
 from openai.embeddings_utils import cosine_similarity, get_embedding
 
 
-filePath = "./speech-transcript.pdf"
-outputName = "output.csv"
-
-
-def get_embedding(text, model="text-embedding-ada-002"):
-    return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
-
-
-def search(df, searchTerm, n=3, pprint=True):
-    queryEmbedding = get_embedding(searchTerm, model='text-embedding-ada-002')
-    df['similarities'] = df.embedding.apply(
-        lambda x: cosine_similarity(x, queryEmbedding))
-    res = df.sort_values('similarities', ascending=False).head(n)
-    return res
-
-
 def main():
-    while True:
-        command = input("Enter a command: ")
-        if int(command) == 1:
-            """ First ypu want to get the sentences from the file with the contents """
-            sentences = utils.get_sentences(filePath)
-            """ Then you want to get the embeddings for each sentence and place it in a pandas dataframe """
-            # text_embedding = get_embedding(sentences[0])
-
-            columns = ["sentence", "embedding"]
-            dataframe = pd.DataFrame(columns=columns)
-            print(sentences[0])
-            dataframe["sentence"] = sentences
-            dataframe["embedding"] = dataframe["sentence"].apply(get_embedding)
-
-            dataframe.to_csv(outputName, index=False)
-
-            print("Done! Check the output file: ", outputName)
-        elif int(command) == 2:
-            """ Read the output file and get the embeddings """
-            dataframe = pd.read_csv(outputName)
-
-            """ Get the search term """
-            searchTerm = input("Enter a search term: ")
-            dataframe['embedding'] = dataframe.embedding.apply(
-                eval).apply(np.array)
-            res = search(dataframe, searchTerm)
-
-            print(res)
-
-        elif int(command) == 0:
-            break
+    print("Welcome to the Party")
 
 
 if __name__ == "__main__":
