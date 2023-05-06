93d3183a21a930a5c579bde6a16013c03cb0edc1
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-05-06 17:04:56 +0300

    added requirements in the requirements file

--- ui/src/components/loading-spinner.tsx
+++ ui/src/components/loading-spinner.tsx
@@ -1,5 +1,10 @@
 import { FunctionComponent } from 'react';
 
 export const LoadingSpinner: FunctionComponent = () => {
-	return <div className="lds-hourglass"></div>;
+	return (
+		<div className="lds-ripple">
+			<div className="border-[4px] border-solid dark:border-white border-black"></div>
+			<div className="border-[4px] border-solid dark:border-white border-black"></div>
+		</div>
+	);
 };
